8152b044fe2e308512c443aa318ebbdf8b7713b2
Agustin <agustindelossantos023@gmail.com>
2025-10-22 19:37:04 +0200

    ADD formGroup, formBuilder

--- src/app/reactive/pages/basic-p/basic-p.ts
+++ src/app/reactive/pages/basic-p/basic-p.ts
@@ -1,12 +1,28 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-p',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-p.html',
   styleUrl: './basic-p.css'
 })
 export class BasicP {
+
+  fb = inject(FormBuilder)
+
+  myForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  })
+
+
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // })
 
 }
